09962c4a022607b1dfb1236ff57ec294695a8e49
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-08-25 14:11:48 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution in this file!
+// Objects Lab
+//jason
+
+
+const driver = {
+    name: "Sam",
+    streetAddress: "5 Main Street",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) 
+  function updateDriverWithKeyAndValue(driver, key, value) {
+    
+    
+    return Object.assign({}, employee, { [key]: value });
+    return Object.assign({}, driver, { [key]: value });
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) 
+    employee[key] = value;
+  function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+  
+    return employee;
+    return driver;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) 
+  function deleteFromDriverByKey(driver, key) {
+
+    const Obj = Object.assign({}, employee);
+    const newObj = Object.assign({}, driver);
+  
+    delete newObj[key];
+  
+    return newObj;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  function destructivelyDeleteFromDriverByKey(driver, key) 
+    delete driver[key];
+  
+    return employee;
+    return driver;
+     }
